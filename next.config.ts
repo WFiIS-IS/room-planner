0a7946a2603d5dfcdfda6cf30a19425a15186d95
Szymon Kozioł <szymonkoziol3@gmail.com>
2024-12-30 18:43:58 +0100

    Fix build

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ['pino', 'pino-pretty'],
   output: 'standalone',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   experimental: {
     turbo: {
       rules: {
