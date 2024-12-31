64f0df0935b62feebee3110bcfbc598de107d86b
Szymon Kozioł <szymonkoziol3@gmail.com>
2024-12-31 18:41:41 +0100

    Fixed global app page

--- src/modules/globals/def.ts
+++ src/modules/globals/def.ts
@@ -2,8 +2,10 @@ import 'server-only';
 
 import { isDev } from '@/env/common';
 
+import { name, version } from '../../../package.json';
+
 export const globals = {
-  projectName: process.env.npm_package_name,
-  projectVersion: process.env.npm_package_version,
+  projectName: name,
+  projectVersion: version,
   isDev,
 } as const;
