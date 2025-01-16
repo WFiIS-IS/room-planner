a69ac8808f5f65d7b4706bd186e684492df7d9c3
MichaelAngeloTheFirst <siemic13@gmail.com>
2025-01-16 11:27:59 +0100

    db operation for adding new position(x,y) for dnd elements

--- src/actions/dndactions.ts
+++ src/actions/dndactions.ts
@@ -1,10 +1,18 @@
 'use server';
 
+import { and, eq } from 'drizzle-orm';
+
 import { db } from '@/lib/db/client';
-import { elementPositions } from '@/lib/db/schema';
+import { elementPositions, scene, scenesToElements } from '@/lib/db/schema';
 
 export default function checkElementPositionExists(sceneSlug: string, elementId: string) {
-  //   todo: implement
+  // if sceneSlug and elementID exists in db return true
+  return db
+    .select()
+    .from(elementPositions)
+    .innerJoin(scenesToElements, eq(scenesToElements.elementUid, elementPositions.elementUid))
+    .innerJoin(scene, eq(scene.slug, scenesToElements.sceneSlug))
+    .where(and(eq(elementPositions.elementUid, elementId), eq(scene.slug, sceneSlug)));
 }
 
 export async function submitElementPosition(
@@ -13,10 +21,17 @@ export async function submitElementPosition(
   position: { x: number; y: number },
 ) {
   return await db.transaction(async (tx) => {
-    await tx.insert(elementPositions).values({
-      elementUid: elementId,
-      x: position.x,
-      y: position.y,
-    });
+    if (await checkElementPositionExists(sceneSlug, elementId)) {
+      return await tx.update(elementPositions).set({
+        x: position.x,
+        y: position.y,
+      });
+    } else {
+      return await tx.insert(elementPositions).values({
+        elementUid: elementId,
+        x: position.x,
+        y: position.y,
+      });
+    }
   });
 }
